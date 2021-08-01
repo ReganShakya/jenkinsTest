ac2994fecdcbecd628f3f361a19a3f495a4f6882
ReganShakya <reganshakya@gmail.com>
2021-08-01 16:05:54 +0545

    Commit

---
+++ src/main/java/np/com/moco/jenkins/jenkinsproject/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package np.com.moco.jenkins.jenkinsproject;
+
+/**
+ *
+ * @author Regan Shakya <regan@moco.com.np>
+ * @Creation Date Aug 1, 2021 4:03:24 PM
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
